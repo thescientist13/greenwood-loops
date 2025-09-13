27b40d936e16eb6f74f87aacb9c31353b44a0668
Owen Buckley <owenbuckley13@gmail.com>
2025-09-13 19:31:35 -0400

    bump v0.33.0

--- src/pages/about.ts
+++ src/pages/about.ts
@@ -1,8 +1,12 @@
-import { type Compilation, type Page, type GetFrontmatter } from "@greenwood/cli";
+import { type Page } from "@greenwood/cli";
 
 const html: string = `
   <h2>About Page</h2>
 `;
+
+const pages: Page[] = [];
+
+console.log({ pages });
 
 export default class AboutPage extends HTMLElement {
   connectedCallback() {
